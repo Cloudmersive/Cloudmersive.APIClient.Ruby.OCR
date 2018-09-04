72183c38c6b87b9f758ead75254afc24146b78a1
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-09-03 18:52:34 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 ﻿Remove-Item –path ./client –recurse
 Remove-Item *.gem
 Remove-Item ./client/*.gem
-& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/nlp -l ruby -o client -c packageconfig.json
+& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/ocr -l ruby -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 
-(Get-Content ./client/cloudmersive-nlp-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select{|f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-nlp-api-client.gemspec
+(Get-Content ./client/cloudmersive-ocr-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select{|f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-ocr-api-client.gemspec
 #& npm build ./client
 
 
@@ -14,6 +14,6 @@ Remove-Item ./client/*.gem
 Push-Location
 & cd ./client
 
-& gem build ./cloudmersive-nlp-api-client.gemspec
+& gem build ./cloudmersive-ocr-api-client.gemspec
 
 Pop-Location
