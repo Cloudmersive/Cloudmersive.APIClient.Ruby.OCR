99b0d0d0934b460a303fa90449e40204c054f130
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-03 21:11:47 -0800

    1.4.9

--- client/lib/cloudmersive-ocr-api-client/version.rb
+++ client/lib/cloudmersive-ocr-api-client/version.rb
@@ -11,5 +11,5 @@ Swagger Codegen version: 2.3.1
 =end
 
 module CloudmersiveOcrApiClient
-  VERSION = "1.4.8"
+  VERSION = "1.4.9"
 end
--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,1 @@
-& gem push .\client\cloudmersive-ocr-api-client-1.4.8.gem
+& gem push .\client\cloudmersive-ocr-api-client-1.4.9.gem
